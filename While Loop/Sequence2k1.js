c5cfa1a81eb404e9bf3130eeacffeecd0b3e572b
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-04 22:14:20 +0200

    Create Sequence2k1.js
    83/100

---
+++ While Loop/Sequence2k1.js
@@ -0,0 +1,10 @@
+function name(input) {
+      let n = Number(input[0]);
+      let num = 1;
+      console.log(num);
+      while (2 * num + 1 <= n) {
+                num = num * 2 + 1;
+            console.log(num);
+            }
+}
+//name(["3"]);                                                                                                                                                          
