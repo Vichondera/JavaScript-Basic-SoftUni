adf935ca19da1400a438c3c58490f2d3a9d0065c
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-04 22:07:15 +0200

    Create MultiplicationTable.js

---
+++ For Loops/MultiplicationTable.js
@@ -0,0 +1,8 @@
+function result(input) {
+    let num = Number(input[0]);
+    for (let i = 1; i <= 10; i++) {
+        let output = num * i;
+        console.log(`${i} * ${num} = ${output}`);
+    }
+}
+//input(["5"]);
