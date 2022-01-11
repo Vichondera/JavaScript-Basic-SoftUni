6ab58931ee215a3d7b7cf0b1509ca2a075822800
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-12 00:03:41 +0200

    2. String Length - Lab
    2. String Length
    Write a JS function that takes three string arguments as an input. 
    Calculate the sum of the length of the strings 
    and the average length of the strings rounded down to the nearest integer.
    The input comes as three string arguments passed to your function.
    The output should be printed on the console in two lines.

---
+++ JS-Advanced/Syntax, Functions and Statements/String Length.js
@@ -0,0 +1,8 @@
+function strLenght(first,sec,thrd) {
+    
+let lenSum = first.length + sec.length + thrd.length;
+console.log(lenSum);
+console.log(`${Math.floor(lenSum / 3)}`);
+  
+}
+//strLenght('chocolate', 'ice cream', 'cake');
