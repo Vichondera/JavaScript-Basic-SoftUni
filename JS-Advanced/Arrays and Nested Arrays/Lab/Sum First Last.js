7ae0940e40bc6e60abb233d8751fa1b414d34058
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-14 00:18:25 +0200

    3. Sum First Last
    Write a function that calculates and returns the sum of the first and the last elements in an array.
    The input comes as an array of string elements holding numbers.
    The output is the return value of your function and should be a number.

---
+++ JS-Advanced/Arrays and Nested Arrays/Lab/Sum First Last.js
@@ -0,0 +1,9 @@
+function sumFirstLast(array) {
+
+    let a = Number(array[0]);
+    let idx = array.length - 1;
+    let b = Number(array[idx]);
+    
+    console.log(a + b);
+}
+// sumFirstLast(['20', '30', '40']);
