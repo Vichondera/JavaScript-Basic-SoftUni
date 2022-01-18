dbb5f2a8e34c6f54081b59cefd2eb7096995e855
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-18 20:49:54 +0200

    01 City Record

---
+++ JS-Advanced/Objects and Composition/City Record.js
@@ -0,0 +1,11 @@
+function cityRecord(city,population,treasury) {
+
+    const object = {
+        name: city,
+        population: population,
+        treasury: treasury
+    };
+
+    return object;
+}
+//cityRecord('Tortuga',7000,15000);
