d5e7823bd48baa4c1a187b1f651e670af9e2ced0
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-19 15:28:56 +0200

    2 Last K Numbers Sequence

---
+++ JS-Advanced/Arrays and Nested Arrays/Lab/Last K Numbers Sequence.js
@@ -0,0 +1,10 @@
+function lastNum(n, k) {
+
+    const arr = [1];
+    
+    for (let i = 1; i < n; i++) {
+        arr.push(arr.slice(-k).reduce((a, v) => a + v, 0));
+    }
+    return arr;
+}
+// lastNum(6, 3);
