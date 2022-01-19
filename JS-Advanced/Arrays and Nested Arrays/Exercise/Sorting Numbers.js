88c642aade9da0bd6a3bedd0b3a5c8fed6713d75
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-19 15:45:20 +0200

    7 Sorting Numbers

---
+++ JS-Advanced/Arrays and Nested Arrays/Exercise/Sorting Numbers.js
@@ -0,0 +1,10 @@
+function sortNums(arr) {
+
+    let i = arr.length - 1;
+    
+    return arr.sort((a, b) => a - b).reduce((a, v, _, tArr) => {
+            a.push(v);
+            a.push(tArr[i--]);
+            return a;
+        }, []).slice(0, arr.length);
+}
