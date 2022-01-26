80c638540d0f8cb6d24d66ac36c81e884b7850ca
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-01-26 23:08:19 +0200

    Create ColorizeTable.js

---
+++ JS-Advanced/DOM Introduction/Lab/05 Colorize Table/ColorizeTable.js
@@ -0,0 +1,13 @@
+function colorize() {
+   
+    let rowElements = document.getElementsByTagName('tr');
+
+    // Convert to arr
+    let rows = Array.from(rowElements);
+
+    rows.forEach((x, i) => {
+        if (i % 2 != 0) {
+            x.style.backgroundColor = 'teal';
+        }
+    });
+}
