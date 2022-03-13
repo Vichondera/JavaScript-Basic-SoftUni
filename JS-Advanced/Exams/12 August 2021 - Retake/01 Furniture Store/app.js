8df79739ee710339ca84b198f36747b0379a62bf
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-03-14 00:56:00 +0200

    Update app.js

--- JS-Advanced/Exams/12 August 2021 - Retake/01 Furniture Store/app.js
+++ JS-Advanced/Exams/12 August 2021 - Retake/01 Furniture Store/app.js
@@ -1,64 +1,90 @@
+window.addEventListener('load', solve);
+
 function solve() {
-   //Todo: Write your code here!
-   const section = document.getElementsByTagName('section')[1];
-   const createBtn = document.getElementsByTagName('button')[0];
-   const ol = document.getElementsByTagName('ol')[0];
+    let modelEl = document.getElementById('model');
+    let yearEl = document.getElementById('year');
+    let descriptionEl = document.getElementById('description');
+    let priceEl = document.getElementById('price');
 
-   createBtn.addEventListener('click', e => {
-      e.preventDefault();
-      let creator = document.getElementById('creator').value;
-      let title = document.getElementById('title').value;
-      let category = document.getElementById('category').value;
-      let content = document.getElementById('content').value;
-   
-      let delBtn = document.createElement('button');
-      delBtn.className = 'btn delete';
-      delBtn.textContent = 'Delete';
+    let addBtnEl = document.getElementById('add');
 
-      let archiveBtn = document.createElement('button');
-      archiveBtn.className = 'btn archive';
-      archiveBtn.textContent = 'Archive';
+    addBtnEl.addEventListener('click', (el) => {
+        el.preventDefault();
 
-      let div = document.createElement('div');
-      div.classList.add('buttons');
-      div.appendChild(delBtn);
-      div.appendChild(archiveBtn);
+        let model = modelEl.value;
+        let year = Number(yearEl.value);
+        let desc = descriptionEl.value;
+        let price = Number(priceEl.value);
 
-      let h1 = document.createElement('h1');
-      h1.textContent = title;
+        if (!model || !desc || !year || !price) {
+            return;
+        }
 
-      let p1 = document.createElement('p');
-      p1.innerHTML = `Category: <strong>${category}</strong>`;
+        modelEl.value = '';
+        yearEl.value = '';
+        descriptionEl.value = '';
+        priceEl.value = '';
 
-      let p2 = document.createElement('p');
-      p2.innerHTML = `Creator: <strong>${creator}</strong>`;
+        let furnitureList = document.getElementById('furniture-list');
 
-      let p3 = document.createElement('p');
-      p3.textContent = content;
+        let tr1 = document.createElement('tr');
+        tr1.className = 'info';
 
-      let articleEl = document.createElement('article');
-      articleEl.appendChild(h1);
-      articleEl.appendChild(p1);
-      articleEl.appendChild(p2);
-      articleEl.appendChild(p3);
-      articleEl.appendChild(div);
+        let td1 = document.createElement('td');
+        td1.textContent = `${model}`;
+        let td2 = document.createElement('td');
+        td2.textContent = `${price.toFixed(2)}`;
 
-      section.appendChild(articleEl);
+        let td3 = document.createElement('td');
 
-      delBtn.addEventListener('click', e => {
-          articleEl.remove();
-      });
+        let moreBtnEl = document.createElement('button');
+        moreBtnEl.className = 'moreBtn';
+        moreBtnEl.innerText = 'More Info';
 
-      archiveBtn.addEventListener('click', e => {
-          let li = document.createElement('li');
-          li.textContent = title;
-          ol.appendChild(li);
+        let buyBtnEl = document.createElement('button');
+        buyBtnEl.className = 'buyBtn';
+        buyBtnEl.textContent = 'Buy it';
 
-          let allLi = Array.from(ol.children);
-          allLi.sort((a, b) => a.textContent.localeCompare(b.textContent)).forEach(li => {
-              ol.appendChild(li);
-          });
-          articleEl.remove();
-      });
-  });
+        td3.appendChild(moreBtnEl);
+        td3.appendChild(buyBtnEl);
+
+        tr1.appendChild(td1);
+        tr1.appendChild(td2);
+        tr1.appendChild(td3);
+        furnitureList.appendChild(tr1);
+
+        let trListEl = document.createElement('tr');
+        trListEl.className = 'hide';
+
+        let tdYear = document.createElement('td');
+        tdYear.innerText = `Year: ${year}`;
+
+        let tdColspan = document.createElement('td');
+        tdColspan.colspan = 3;
+        tdColspan.textContent = `Description: ${desc}`;
+
+        trListEl.appendChild(tdYear);
+        trListEl.appendChild(tdColspan);
+        furnitureList.appendChild(trListEl);
+
+        moreBtnEl.addEventListener('click', (el) => {
+            moreBtnEl.innerText = 'Less Info';
+            moreBtnEl.style = 'display: contents;';
+        });
+
+        buyBtnEl.addEventListener('click', (el) => {
+            furnitureList.removeChild(furnitureList.lastChild);
+            furnitureList.removeChild(furnitureList.lastChild);
+            moreBtnEl.innerText = 'Less Info';
+            moreBtnEl.style = 'display: contents;';
+            
+            let t = document.getElementsByClassName('total-price');
+            let totalPrice = price + Number(t[0].innerHTML);
+            t[0].innerHTML = `${totalPrice.toFixed(2)}`;
+
+        });
+
+
+    });
+
 }
