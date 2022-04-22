ce5d0a887fa56c9412d8e1896e68f9d3e2450bae
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-22 18:16:24 +0300

    Delete JS-Applications/Exams/Meme Lounge/src directory

--- JS-Applications/Exams/Meme Lounge/src/lib.js
+++
@@ -1,7 +0,0 @@
-import page from '../node_modules/page/page.mjs';
-import { html, render } from '../node_modules/lit-html/lit-html.js';
-
-import { pageBuilder } from './services/pageBuilder.js';
-import { logout, login } from './services/data.js';
-
-export { page, html, render, pageBuilder, login, logout };
