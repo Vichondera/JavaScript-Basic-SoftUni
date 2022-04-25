3525fb018fa7a6996fd3421014150c1caa8780a1
V.Grigorov <51731349+Vichondera@users.noreply.github.com>
2022-04-25 21:22:59 +0300

    Update data.js

--- JS-Applications/Exams/Meme Lounge/src/services/data.js
+++ JS-Applications/Exams/Meme Lounge/src/services/data.js
@@ -6,3 +6,17 @@ auth.settings.host = host;
 export const login = auth.login;
 export const register = auth.register;
 export const logout = auth.logout;
+
+export async function getMemes() {
+    const memes = await auth.get(host + "/data/memes?sortBy=_createdOn%20desc");
+    return memes;
+}
+
+export async function createMeme(title, description, imageUrl) {
+    const result = await auth.post(host + "/data/memes", {
+        title,
+        description,
+        imageUrl
+    });
+    return result;
+}
